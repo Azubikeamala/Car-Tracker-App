bc82e335fb8729274075302b461243459b1869b4
Amalachukwu Azubike <onyenweamala@gmail.com>
2025-05-06 13:51:17 -0400

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -4,14 +4,15 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDa4fn9erZMXXCIgyJMMQoT7zIi8OvKP4",
-  authDomain: "car-tracker-3aeb5.firebaseapp.com",
-  databaseURL: "https://car-tracker-3aeb5-default-rtdb.firebaseio.com",
-  projectId: "car-tracker-3aeb5",
-  storageBucket: "car-tracker-3aeb5.appspot.com",
-  messagingSenderId: "981803314783",
-  appId: "1:981803314783:web:e64effbc2adacec18cb5ea"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
